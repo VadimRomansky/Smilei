dc7a7a95c9dafa18a3c593900ffa5cc734fc9259
vadimromansky <romanskyvadim@gmail.com>
2020-10-12 15:41:40 +0300

    input and output

--- plot_general.m
+++ plot_general.m
@@ -3,8 +3,29 @@ general = importdata('./output/scalars.txt');
 N=size(general,1);
 
 figure(1);
+plot(1:N, general(1:N,1));
+title ('t');
+xlabel ('N');
+ylabel ('t');
+grid;
+
+figure(2);
 plot(general(1:N,1), general(1:N,4));
 title ('Utot');
+xlabel ('t');
+ylabel ('E');
+grid;
+
+figure(3);
+plot(general(1:N,1), general(1:N,6));
+title ('Ukin');
+xlabel ('t');
+ylabel ('E');
+grid;
+
+figure(4);
+plot(general(1:N,1), general(1:N,9));
+title ('Uelm');
 xlabel ('t');
 ylabel ('E');
 grid;
